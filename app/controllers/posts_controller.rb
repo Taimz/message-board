fb43389d85119577c0f29b39e3403b797f525682
Taimur Danish <taimur.danish123@gmail.com>
2022-04-30 01:41:17 -0400

    Added show page and post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,33 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show]
+
   def index
     @posts = Post.order(id: :desc).all
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to post_path(@post), notice: 'Post created successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
